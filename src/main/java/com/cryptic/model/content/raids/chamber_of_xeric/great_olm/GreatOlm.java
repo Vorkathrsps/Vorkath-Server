adcd395658befd8d44021a2981733c497099e9b9
Origin <2007zerkerpure@gmail.com>
2023-11-17 10:37:30 -0600

    Origin:

--- src/main/java/com/cryptic/model/content/raids/chamber_of_xeric/great_olm/GreatOlm.java
+++ src/main/java/com/cryptic/model/content/raids/chamber_of_xeric/great_olm/GreatOlm.java
@@ -177,8 +177,12 @@ public class GreatOlm extends CommonCombatMethod {
     public void preDefend(Hit hit) {
         var player = (Player) hit.getAttacker();
         if (hit.getAttacker() == player) {
-            if (this.currentPhase != lastPhase && !leftClaw.dead() && !rightClaw.dead()) {
-                hit.block();
+            if (this.currentPhase != lastPhase) {
+                if (!leftClaw.dead()) {
+                    if (!rightClaw.dead()) {
+                        hit.block();
+                    }
+                }
             }
         }
     }

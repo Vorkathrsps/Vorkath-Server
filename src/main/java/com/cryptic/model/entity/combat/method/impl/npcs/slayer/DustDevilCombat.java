f1a06cdba319d8a14bb1aaa6284dc280aa2d7748
OriginRsPs <91713517+OriginRsPs@users.noreply.github.com>
2024-04-09 03:16:56 -0500

    - fix for dust devil combat not checking for facemask & slayer helmet variants

--- src/main/java/com/cryptic/model/entity/combat/method/impl/npcs/slayer/DustDevilCombat.java
+++ src/main/java/com/cryptic/model/entity/combat/method/impl/npcs/slayer/DustDevilCombat.java
@@ -3,6 +3,8 @@ package com.cryptic.model.entity.combat.method.impl.npcs.slayer;
 import com.cryptic.model.entity.Entity;
 import com.cryptic.model.entity.combat.CombatFactory;
 import com.cryptic.model.entity.combat.CombatType;
+import com.cryptic.model.entity.combat.formula.FormulaUtils;
+import com.cryptic.model.entity.combat.hit.Hit;
 import com.cryptic.model.entity.combat.method.impl.CommonCombatMethod;
 import com.cryptic.model.entity.player.EquipSlot;
 import com.cryptic.model.entity.player.Player;
@@ -16,7 +18,7 @@ import com.cryptic.utility.Utils;
  */
 public class DustDevilCombat extends CommonCombatMethod {
 
-    private static final int[] DRAIN = { Skills.ATTACK, Skills.STRENGTH, Skills.RANGED, Skills.MAGIC};
+    private static final int[] DRAIN = {Skills.ATTACK, Skills.STRENGTH, Skills.RANGED, Skills.MAGIC};
 
     private void basicAttack(Entity entity, Entity target) {
         target.hit(entity, CombatFactory.calcDamageFromType(entity, target, CombatType.MELEE), 0, CombatType.MELEE).checkAccuracy(true).submit();
@@ -26,16 +28,13 @@ public class DustDevilCombat extends CommonCombatMethod {
     @Override
     public boolean prepareAttack(Entity entity, Entity target) {
         Player player = (Player) target;
-        if ((player.getEquipment().getId(EquipSlot.HEAD) != ItemIdentifiers.FACEMASK && !player.getEquipment().wearingSlayerHelm())) {
-            player.hit(entity, 16, CombatType.MELEE).submit();
+        if ((!player.getEquipment().contains(ItemIdentifiers.FACEMASK) || !FormulaUtils.hasSlayerHelmet(player) || !FormulaUtils.hasSlayerHelmetImbued(player))) {
+            new Hit(entity, target, 0, CombatType.MELEE).checkAccuracy(true).submit();
             player.message("<col=ff0000>The devil's dust blinds and damages you!");
             player.message("<col=ff0000>A facemask can protect you from this attack.");
-        }
-        basicAttack(entity, target);
-        if(Utils.rollDie(5, 1)) {
-            for (int skill : DRAIN) {
-                player.getSkills().alterSkill(skill, -5);
-            }
+            for (int skill : DRAIN) player.getSkills().alterSkill(skill, -5);
+        } else {
+            basicAttack(entity, target);
         }
         return true;
     }

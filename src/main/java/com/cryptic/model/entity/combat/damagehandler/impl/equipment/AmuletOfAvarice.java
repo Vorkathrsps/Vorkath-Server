f646ca5c87a69967831528ce088873e42057f244
OriginRsPs <91713517+OriginRsPs@users.noreply.github.com>
2024-05-10 10:27:26 -0500

    - .

--- src/main/java/com/cryptic/model/entity/combat/damagehandler/impl/equipment/AmuletOfAvarice.java
+++ src/main/java/com/cryptic/model/entity/combat/damagehandler/impl/equipment/AmuletOfAvarice.java
@@ -8,8 +8,10 @@ import com.cryptic.model.entity.combat.damagehandler.listener.DamageModifyingLis
 import com.cryptic.model.entity.combat.formula.FormulaUtils;
 import com.cryptic.model.entity.combat.formula.accuracy.AbstractAccuracy;
 import com.cryptic.model.entity.combat.hit.Hit;
+import com.cryptic.model.entity.npc.NPC;
 import com.cryptic.model.entity.player.Player;
 import com.cryptic.utility.ItemIdentifiers;
+import org.apache.commons.lang.ArrayUtils;
 
 public class AmuletOfAvarice implements DamageModifyingListener {
     @Override
@@ -17,24 +19,37 @@ public class AmuletOfAvarice implements DamageModifyingListener {
         if (entity instanceof Player player) {
             SlayerTask task = World.getWorld().getSlayerTasks();
             SlayerTask assignment = task.getCurrentAssignment(player);
-            if (!player.getEquipment().contains(ItemIdentifiers.AMULET_OF_AVARICE)) return false;
-            if (((FormulaUtils.hasSlayerHelmet(player) || FormulaUtils.hasSlayerHelmetImbued(player)) && assignment != null)) return false;
-            int damage = hit.getDamage();
-            hit.setDamage((int) (damage * 1.20D));
-            return true;
+            final Entity target = player.getCombat().getTarget();
+            if (target == null) return false;
+            if (target instanceof NPC npc) {
+                if (!player.getEquipment().contains(ItemIdentifiers.AMULET_OF_AVARICE)) return false;
+                if (((FormulaUtils.hasSlayerHelmet(player) || FormulaUtils.hasSlayerHelmetImbued(player)) && assignment != null && ArrayUtils.contains(assignment.getNpcs(), npc.id())))
+                    return false;
+                if (!ArrayUtils.contains(FormulaUtils.isRevenant(), npc.id())) return false;
+                int damage = hit.getDamage();
+                hit.setDamage((int) (damage * 1.20D));
+                return true;
+            }
         }
         return false;
     }
+
     @Override
     public double prepareAccuracyModification(Entity entity, CombatType combatType, AbstractAccuracy accuracy) {
         double boost = 0.0D;
         if (entity instanceof Player player) {
             SlayerTask task = World.getWorld().getSlayerTasks();
             SlayerTask assignment = task.getCurrentAssignment(player);
-            if (!player.getEquipment().contains(ItemIdentifiers.AMULET_OF_AVARICE)) return boost;
-            if (((FormulaUtils.hasSlayerHelmet(player) || FormulaUtils.hasSlayerHelmetImbued(player)) && assignment != null)) return boost;
-            boost = 1.20D;
-            return boost;
+            final Entity target = player.getCombat().getTarget();
+            if (target == null) return boost;
+            if (target instanceof NPC npc) {
+                if (!player.getEquipment().contains(ItemIdentifiers.AMULET_OF_AVARICE)) return boost;
+                if (((FormulaUtils.hasSlayerHelmet(player) || FormulaUtils.hasSlayerHelmetImbued(player)) && assignment != null && ArrayUtils.contains(assignment.getNpcs(), npc.id())))
+                    return boost;
+                if (!ArrayUtils.contains(FormulaUtils.isRevenant(), npc.id())) return boost;
+                boost = 1.20D;
+                return boost;
+            }
         }
         return boost;
     }

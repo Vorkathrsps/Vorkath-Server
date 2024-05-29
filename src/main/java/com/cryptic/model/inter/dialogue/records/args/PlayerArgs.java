159e5ff4a215ed3d279b6801f13d11df85d56854
Mark7625 <72366279+Mark7625@users.noreply.github.com>
2024-05-29 02:29:07 +0100

    Update PlayerArgs.java

--- src/main/java/com/cryptic/model/inter/dialogue/records/args/PlayerArgs.java
+++ src/main/java/com/cryptic/model/inter/dialogue/records/args/PlayerArgs.java
@@ -1,5 +1,6 @@
 package com.cryptic.model.inter.dialogue.records.args;
 
+import com.cryptic.GameServer;
 import com.cryptic.model.cs2.ComponentID;
 import com.cryptic.model.entity.player.Player;
 import com.cryptic.model.inter.dialogue.Expression;
@@ -10,7 +11,8 @@ public record PlayerArgs(String title, String[] chats, Expression expression, bo
     public void send(Player player) {
         player.getPacketSender().setPlayerHeadMesssage(ComponentID.PLAYER_CHAT_HEAD);
         player.getPacketSender().setAnimMessage(ComponentID.PLAYER_CHAT_HEAD, this.expression().getAnimation().getId());
-        player.getPacketSender().setComponentText(ComponentID.PLAYER_CHAT_TITLE, this.title().isBlank() ? this.title() : player.getUsername());
+        String finalTitle = this.title.isEmpty() ? player.getUsername() : this.title;
+        player.getPacketSender().setComponentText(ComponentID.PLAYER_CHAT_TITLE, finalTitle);
         player.getPacketSender().setComponentText(ComponentID.PLAYER_CHAT_MESSAGE, Utils.joinWithBr(chats));
         player.getPacketSender().setComponentVisability(ComponentID.PLAYER_CHAT_CONTINUE, !this.continueButtons);
         player.getPacketSender().setComponentText(ComponentID.PLAYER_CHAT_CONTINUE, "Click here to continue");

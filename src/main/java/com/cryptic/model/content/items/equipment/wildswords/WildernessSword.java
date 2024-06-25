19db846b69b0c0fdbc9c15f5ad5838b2f565af46
OriginRsPs <91713517+OriginRsPs@users.noreply.github.com>
2024-06-25 13:53:48 -0500

    - wilderness sword teleports now have a dialogue prompt letting you know its a dangerous teleport

--- src/main/java/com/cryptic/model/content/items/equipment/wildswords/WildernessSword.java
+++ src/main/java/com/cryptic/model/content/items/equipment/wildswords/WildernessSword.java
@@ -4,6 +4,8 @@ import com.cryptic.model.content.teleport.TeleportType;
 import com.cryptic.model.content.teleport.Teleports;
 import com.cryptic.model.entity.masks.impl.animations.Animation;
 import com.cryptic.model.entity.player.Player;
+import com.cryptic.model.inter.dialogue.Dialogue;
+import com.cryptic.model.inter.dialogue.DialogueType;
 import com.cryptic.model.items.Item;
 import com.cryptic.model.map.position.Tile;
 import com.cryptic.network.packet.incoming.interaction.PacketInteraction;
@@ -16,31 +18,53 @@ public class WildernessSword extends PacketInteraction {
         if (option == 3) {
             if (item.getId() == ItemIdentifiers.WILDERNESS_SWORD_3) {
                 if (Teleports.canTeleport(player, true, TeleportType.ABOVE_20_WILD)) {
-                    player.lockMovement();
-                    player.animate(3872);
-                    player.graphic(283);
-                    Chain.noCtx().runFn(4, () -> {
-                       player.teleport(new Tile(2998, 3913));
-                       player.animate(Animation.DEFAULT_RESET_ANIMATION);
-                       player.unlock();
-                    });
+                    handleTeleport(player, "50", 2998, 3913);
                 }
                 return true;
             }
             if (item.getId() == ItemIdentifiers.WILDERNESS_SWORD_4) {
                 if (Teleports.canTeleport(player, true, TeleportType.ABOVE_20_WILD)) {
-                    player.lockMovement();
-                    player.animate(3872);
-                    player.graphic(283);
-                    Chain.noCtx().runFn(4, () -> {
-                        player.teleport(new Tile(3018, 3958));
-                        player.animate(Animation.DEFAULT_RESET_ANIMATION);
-                        player.unlock();
-                    });
+                    handleTeleport(player, "55",3018, 3958);
                 }
                 return true;
             }
         }
         return false;
+    }
+
+    private static void handleTeleport(Player player, String wildernessLevel, int x, int y) {
+        player.getDialogueManager().start(new Dialogue() {
+            @Override
+            protected void start(Object... parameters) {
+                send(DialogueType.STATEMENT, "This Teleport will take you into Level " + wildernessLevel + " Deep Wilderness.");
+                setPhase(0);
+            }
+
+            @Override
+            protected void next() {
+                if (isPhase(0)) {
+                    send(DialogueType.OPTION, "Would you like to continue?", "Yes", "No");
+                }
+            }
+
+            @Override
+            protected void select(int option) {
+                if (isPhase(0)) {
+                    if (option == 1) {
+                        player.lockMovement();
+                        player.animate(3872);
+                        player.graphic(283);
+                        Chain.noCtx().runFn(4, () -> {
+                            player.teleport(new Tile(x, y));
+                            player.animate(Animation.DEFAULT_RESET_ANIMATION);
+                            player.unlock();
+                        });
+                        stop();
+                    } else {
+                        stop();
+                    }
+                }
+            }
+        });
     }
 }

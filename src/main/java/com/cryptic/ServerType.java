758ca40d811c65f0ec58b380d9e57b8502428219
Mark7625 <72366279+Mark7625@users.noreply.github.com>
2024-06-27 02:57:10 +0100

    Update ServerType.java

--- src/main/java/com/cryptic/ServerType.java
+++ src/main/java/com/cryptic/ServerType.java
@@ -3,6 +3,7 @@ package com.cryptic;
 import com.cryptic.model.map.position.Tile;
 import lombok.Getter;
 
+@Getter
 public enum ServerType {
     VORKATH("Vorkath",new Tile(3097, 3501, 0)),
     VARLAMORE("Varlamore",new Tile(1647, 3108, 0));
@@ -12,10 +13,8 @@ public enum ServerType {
         this.homeTile = homeTile;
     }
 
-    @Getter
     private final String name;
 
-    @Getter
     private final Tile homeTile;
 
 }
